4b8c527f6e47e0dcf5c02bc7c4319db233cb8dd1
Daniel Azevedo <daniel.azevedo.maia@hotmail.com>
2024-01-20 22:52:15 -0300

    Reorganização de diretórios em backend e frontend

---
+++ backend/src/main/java/com/daniel/azevedo/meuwebsite/MeuWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.daniel.azevedo.meuwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuWebsiteApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/daniel/azevedo/meuwebsite/MeuWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.azevedo.meuwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
