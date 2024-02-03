d60a4a82a10ba1056092ba594b967fa5b4d2af00
Daniel Azevedo <daniel.azevedo.maia@hotmail.com>
2024-02-03 19:07:26 -0300

    Criação de TODOLIST para exceptions

--- backend/src/main/java/net/daniel/azevedo/meuwebsite/repository/PostRepository.java
+++ backend/src/main/java/net/daniel/azevedo/meuwebsite/repository/PostRepository.java
@@ -1,10 +1,15 @@
 package net.daniel.azevedo.meuwebsite.repository;
 
 import net.daniel.azevedo.meuwebsite.domain.Post;
+import net.daniel.azevedo.meuwebsite.dto.usuario.PostResponseDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+
 
 }
