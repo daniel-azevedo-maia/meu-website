e83a8d07c20fe1e8a7ec81eeda3ceac17338e32a
Daniel Azevedo <daniel.azevedo.maia@hotmail.com>
2024-08-14 21:27:16 -0300

    Novo front-end

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,20 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FaleComigoComponent } from './features/fale-comigo/fale-comigo.component';
-import { QuemSouEuComponent } from './features/quem-sou-eu/quem-sou-eu.component';
-import { EmConstrucaoComponent } from './features/em-construcao/em-construcao.component';
-import { NovoPostComponent } from './features/novo-post/novo-post.component';
-import { HomeComponent } from './features/home/home.component';
+
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'direito', component: EmConstrucaoComponent },
-  { path: 'tecnologia', component: EmConstrucaoComponent },
-  { path: 'opinioes', component: EmConstrucaoComponent },
-  { path: 'quem-sou-eu', component: QuemSouEuComponent },
-  { path: 'fale-comigo', component: FaleComigoComponent },
-  { path: 'novo-post', component: NovoPostComponent }
+
   
 ];
 
--- frontend/src/app/features/features.module.ts
+++ frontend/src/app/features/features.module.ts
@@ -1,24 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DireitoComponent } from './direito/direito.component';
-import { EmConstrucaoComponent } from './em-construcao/em-construcao.component';
-import { FaleComigoComponent } from './fale-comigo/fale-comigo.component';
-import { HomeComponent } from './home/home.component';
-import { OpinioesComponent } from './opinioes/opinioes.component';
-import { QuemSouEuComponent } from './quem-sou-eu/quem-sou-eu.component';
-import { NovoPostComponent } from './novo-post/novo-post.component';
+
 import { FormsModule } from '@angular/forms';
 import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
 
 @NgModule({
   declarations: [
-    DireitoComponent,
-    EmConstrucaoComponent,
-    FaleComigoComponent,
-    HomeComponent,
-    OpinioesComponent,
-    QuemSouEuComponent,
-    NovoPostComponent
+
   ],
   imports: [
     CommonModule,
@@ -26,13 +14,7 @@ import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
     CKEditorModule
   ],
   exports: [
-    DireitoComponent,
-    EmConstrucaoComponent,
-    FaleComigoComponent,
-    HomeComponent,
-    OpinioesComponent,
-    QuemSouEuComponent,
-    NovoPostComponent
+
   ]
 
 })
